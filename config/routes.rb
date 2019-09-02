3bcdfa10de36961554d8c54c1be193bcbaa3f626
developer <ctkrecordings@gmail.com>
2019-09-02 17:55:34 -0600

    ADDED: user login with bcrypt

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
 
+  get 'signup', to: 'users#new'
+
   resources :articles
+  resources :users, except: [:new]
 end
